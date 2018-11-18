1f73424cf8625265b0ba55992121e3e108896aaf
Krystian Solarz <krystek20@me.com>
2018-11-18 19:46:11 +0100

    Initial Commit

---
+++ R3PI_MagicBasket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R3PI_MagicBasket
+//
+//  Created by Krystian Solarz on 18/11/2018.
+//  Copyright © 2018 Krystian Solarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
